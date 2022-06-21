6ba42dd6708a7f2cb335526286c63a5d11e68d77
MohammadGhaderi0 <107918334+MohammadGhaderi0@users.noreply.github.com>
2022-06-21 17:54:59 +0430

    Add files via upload
    Just a test ! dont pay attention

---
+++ GuiAgain.java
@@ -0,0 +1,31 @@
+
+package gui.again;
+
+import java.awt.Color;
+import javax.swing.*;
+
+public class GuiAgain {
+    public GuiAgain(){
+    
+}
+
+    public static void main(String[] args) {
+        /*JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(420,420);
+        frame.setTitle("15puzzle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        ImageIcon logo = new ImageIcon("images.png");
+        frame.setIconImage(logo.getImage());
+        frame.getContentPane().setBackground(Color.gray);
+        */
+        Myframe frame = new Myframe();
+        
+        
+        
+        
+    
+    }
+    
+}
