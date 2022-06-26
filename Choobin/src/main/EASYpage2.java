f0b66946725d4e6cf37b32558845070dcb2a87b8
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-06-26 20:59:36 +0430

    Update EASYpage2.java

--- Choobin/src/main/EASYpage2.java
+++ Choobin/src/main/EASYpage2.java
@@ -1,26 +1,34 @@
+
 package main;
 
 import java.awt.*;
 import javax.swing.*;
 
-public class EASYpage2 extends JPanel{
+public class EASYpage2 extends JFrame{ // بجاي پنل از فريم استفاده ميکنيم
+
     public JButton three;
     public JButton four;
     public JButton five;
-    
+
     public EASYpage2(){
-        this.setSize(500,700);
-        this.setVisible(true);
-        
-        this.setLayout(new FlowLayout());
-        this.setPreferredSize(new Dimension(500, 700));
-        three = new JButton();    
-        four = new JButton();
-        five = new JButton();
-        this.add(three);
-        this.add(four);
-        this.add(five);
-        this.setVisible(true);
-        this.setBackground(Color.pink);
+        super("Welcom to Choobin Pazel"); // نام تايتل ما
+
+        setSize(500,700);        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // اين رو حتما بنويس تا با ضربدر قرمز بسته شه
+
+        setLayout(null); 
+        //setPreferredSize(new Dimension(500, 700));
+        setLocationRelativeTo(null); // اين رو حتما بنويس تا بياره مرکز
+        three = new JButton("3*3");    
+        four = new JButton("4 x 4");
+        five = new JButton("5x5");        
+        getContentPane().setBackground(Color.GREEN); 
+        three.setBounds(100,100,100,100);
+        four.setBounds(100,2,100,100);
+        five.setBounds(2,100,100,100);
+        add(three);        
+        add(four);
+        add(five);
+        setVisible(true);
     }
 }
