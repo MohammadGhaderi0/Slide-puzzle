4c66c685dbc3b280fd4dbb6a41832e0a492fb55c
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-06-28 13:50:13 +0430

    Add files via upload

---
+++ Choobin/src/main/RecordHa.java
@@ -0,0 +1,65 @@
+
+package main;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class RecordHa extends  JFrame{  
+    
+// AjZaye Darone Safhe           
+        public JButton Khane; 
+        public JTable Recordha;                    
+        public String[] RadifeAval=new String[] {"زمان","نام"};
+        
+// Sazandeye Frame      
+        public RecordHa(String[][] BaghiyeRadifHa)  {        
+                    
+                    super("Welcom to Choobin  Pazel"); 
+              
+// Andaze Frame
+                    setSize(500,700);
+                    
+// Miare Markaz Safhe ==> https://stackoverflow.com/questions/144892/how-to-center-a-window-in-java
+                    setLocationRelativeTo(null);
+                    
+ // Chegoonegie Baste Shodan Safhe
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+                    
+// Sakhte Yek Nemone Az Dokmeha Be Hamrahe Nameshoon     
+                    Khane = new JButton("خانه");
+                    Recordha=new JTable(BaghiyeRadifHa,RadifeAval);
+                    
+// Andaze Va Mokhtasate Dokmeha
+                    Khane.setBounds(200,500,100,100);
+                    
+// Halate Focus Ra Az Dokmeha Bar Midarad                 
+                    Khane.setFocusable(false);                    
+                  
+// Ezafe Kardan Ajza Be Frame => Aval Bayad Dokme Ezafe Shavad Ta Be Error Nakhorim                 
+                     add(Khane);
+                     add (new JScrollPane(Recordha));
+
+                     getContentPane().setBackground(Color.BLUE);
+        
+// Ghabeliat Didane Frame      
+                     setVisible(true);    
+
+                     
+// Baraye Dokme Ha Yek Action  Ezafe Mikonam                          
+                      Khane.addActionListener(new ActionListener(){
+                                public void actionPerformed(ActionEvent e){
+                
+                                new SafheyeAval();
+
+// Safheye Ghabli ra Mibandad                
+                                setVisible(false);
+         }
+        });
+                   
+    }
+        
+}
