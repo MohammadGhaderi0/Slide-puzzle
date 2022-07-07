75200a51a39f79995b454e68244d9d45e2825c5d
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-07-07 15:02:13 +0430

    Update Sakht5Dar5.java

--- Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
+++ Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
@@ -11,7 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class Sakht5Dar5 extends CodhayeMoshtarak{
+public class Sakht5Dar5 extends DokmeHa{
 
    
     public JButton btn10;
