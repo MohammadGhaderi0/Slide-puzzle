a101b83df92a03e0f3aa50df6cbcbf8e1e4fc9fb
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-07-08 12:07:46 +0430

    Update Sakht5Dar5.java

--- Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
+++ Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
@@ -389,11 +389,16 @@ public class Sakht5Dar5 extends DokmeHa{
         });
         back.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                int a =JOptionPane.showConfirmDialog(null,"Are you sure?");
+                int a =JOptionPane.showConfirmDialog(null,"خواهش ميکنم خارج نشو!!");
+   
                 if (a ==0){
+                    JOptionPane.showMessageDialog(null, "اي بابا!!");
                     new EASYpage2();
                     setVisible(false);
                     
+                }else{
+                    JOptionPane.showMessageDialog(null, "اي ول!!");
+
                 }
                         }
         });
