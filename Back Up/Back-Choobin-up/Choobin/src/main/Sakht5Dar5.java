20ed808a6c898a8635229c5a61b5a27738b75137
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-07-08 12:14:10 +0430

    Update Sakht5Dar5.java

--- Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
+++ Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+import java.awt.Font;
+
 import static javax.swing.JOptionPane.PLAIN_MESSAGE;
 
 import javax.swing.JButton;
