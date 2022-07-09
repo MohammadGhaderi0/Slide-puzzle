9abacfb3764be74cc0f8f765ae9844a051d54cab
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-07-09 15:59:13 +0430

    Update Sakht3Dar3.java

--- Back Up/Back-Choobin-up/Choobin/src/main/Sakht3Dar3.java
+++ Back Up/Back-Choobin-up/Choobin/src/main/Sakht3Dar3.java
@@ -323,7 +323,7 @@ MakhfiKardaneDokmeha();
          btn9.setBackground(new Color(250, 200, 0));
 
     
-     if (btn8.getText()!=""&&btn1.getText()!=""&&btn2.getText()!=""&&btn3.getText()!=""&&btn4.getText()!=""&&btn5.getText()!=""&&btn6.getText()!=""&&btn7.getText()!=""&&1==Integer.parseInt(btn1.getText())&&2==Integer.parseInt(btn2.getText())&&3==Integer.parseInt(btn3.getText())&&4==Integer.parseInt(btn4.getText())&&5==Integer.parseInt(btn5.getText())&&6==Integer.parseInt(btn6.getText())&&7==Integer.parseInt(btn7.getText())&&8==Integer.parseInt(btn8.getText())){
+     if (btn9.getText()==""&&btn8.getText()!=""&&btn1.getText()!=""&&btn2.getText()!=""&&btn3.getText()!=""&&btn4.getText()!=""&&btn5.getText()!=""&&btn6.getText()!=""&&btn7.getText()!=""&&1==Integer.parseInt(btn1.getText())&&2==Integer.parseInt(btn2.getText())&&3==Integer.parseInt(btn3.getText())&&4==Integer.parseInt(btn4.getText())&&5==Integer.parseInt(btn5.getText())&&6==Integer.parseInt(btn6.getText())&&7==Integer.parseInt(btn7.getText())&&8==Integer.parseInt(btn8.getText())){
         String ssss= JOptionPane.showInputDialog(null, "تو بردي  توخيلي خوبي  حالا اسمت رو بنويس:","نام",PLAIN_MESSAGE);
 
                 System.out.println(ssss);
