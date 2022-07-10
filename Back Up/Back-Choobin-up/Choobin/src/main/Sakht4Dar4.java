cfcca99ff3b18bd790203b1865868efad34ae67a
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-07-10 23:07:52 +0430

    Update Sakht4Dar4.java

--- Back Up/Back-Choobin-up/Choobin/src/main/Sakht4Dar4.java
+++ Back Up/Back-Choobin-up/Choobin/src/main/Sakht4Dar4.java
@@ -541,7 +541,7 @@ public class Sakht4Dar4 extends DokmeHa {
                     start();  
 		                            back.setBounds(310,555,100,50);
                     stop.setBounds(70,555,100,50);
-                    restart.setBounds(70,555,100,50);
+                                        restart.setBounds(190,555,100,50);
                     timeLabel.setBounds(195,470,200,100);
             }}
         });
