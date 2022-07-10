d034e07fda9c03ff8249e57a876ef7c5c535b014
A-h-H-z-F <103555167+A-h-H-z-F@users.noreply.github.com>
2022-07-10 22:58:25 +0430

    Update Sakht5Dar5.java

--- Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
+++ Back Up/Back-Choobin-up/Choobin/src/main/Sakht5Dar5.java
@@ -779,11 +779,19 @@ public class Sakht5Dar5 extends DokmeHa{
                    started=false;
 		   stop.setText("ادامه");
 		   stop();
+		                                  back.setBounds(190,420,100,50);
+                           stop.setBounds(190,240,100,50);
+                            restart.setBounds(190,330,100,50);
+                            timeLabel.setBounds(195,120,200,100);
                }
                else if (e.getSource()==stop&& started == false) {
                     started=true;
                     stop.setText("توقف");
                     start();  
+		                            back.setBounds(310,555,100,50);
+                    stop.setBounds(70,555,100,50);
+                    restart.setBounds(70,555,100,50);
+                    timeLabel.setBounds(195,470,200,100);
             }}
         });
         
